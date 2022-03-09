381b41fe0e635f8658eda39c5c86ae6454485419
rrr <rayanhamok@yahoo.fr>
2022-03-09 16:10:36 +0100

    test

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'inscription',
+    component: InscriptionComponent
+  },
+  {
+    path: 'contact-list',
+    component : ContactListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    InscriptionComponent,
   ],
   imports: [
     BrowserModule,
