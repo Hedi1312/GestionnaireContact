46539988717867796e5b92184af514ae9706c97c
omar SALEK <omarsalek05@gmail.com>
2022-03-10 00:42:46 +0100

    Mise en place de la Page d' Accueil

--- frontend/src/app/composants/accueil/accueil.component.ts
+++ frontend/src/app/composants/accueil/accueil.component.ts
@@ -11,5 +11,12 @@ export class AccueilComponent implements OnInit {
 
   ngOnInit() {
   }
+  myimage:String = "assets/images/iphone.jpg";
+  myimage2:String = "assets/images/bookAdress.jpg";
+  contactImg:String = "assets/images/contact.png";
+  appleMail:String = "assets/images/AppleMail.png";
+  notes:String = "assets/images/notes.png";
+  croix:String = "assets/images/croix.png";
+
 
 }
